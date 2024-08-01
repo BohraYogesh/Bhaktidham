cf9775bb97d5e3fd381d6bd206eefbbdd8fd200c
BohraYogesh <yogeshbohra124@gmail.com>
2024-08-01 22:01:49 +0530

    devi

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './Header.css'
-import headerimg from '../../assets/Hanuman.jpg'
 
 export const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <img src={headerimg} alt="" />
       </div>
     </div>
   )
