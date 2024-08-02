223f01b54e9f1978763dae9f5304a12833d318d6
BohraYogesh <yogeshbohra124@gmail.com>
2024-08-02 12:37:19 +0530

    first commit

---
+++ src/components/Display/Display.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Display = () => {
+  return (
+    <div>Display</div>
+  )
+}
+
+export default Display;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import headerimg from '../../assets/Hanuman.jpg'
 
 export const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
+        <img src={headerimg} alt="" />
       </div>
     </div>
   )
